5e5745816744889f9b2e2443663d4ae562ed4070
Zaeboz <jegs2206@gmail.com>
2022-11-10 22:20:36 -0500

    Nuevas configuraciones al servicio login

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,37 +1,102 @@
 import { Injectable } from '@angular/core';
 
+import {
+  Auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
-import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, signInWithPopup, GoogleAuthProvider } from '@angular/fire/auth';
+import firebase from 'firebase/compat/app';
+
+
 
 @Injectable({
-  providedIn: 'root'
-})
+   providedIn: 'root',
+ })
+
+
+
 export class UserService {
+  sesionIniciada: boolean = false;
 
-  sesionIniciada: boolean = false
+  token: string;
 
-  constructor(private auth: Auth) { }
+
+
+
+
+
+  constructor(private auth: Auth, private router: Router) {}
 
   register({ email, password }: any) {
-    this.sesionIniciada=true;
+    this.sesionIniciada = true;
+    console.log(this.sesionIniciada);
     return createUserWithEmailAndPassword(this.auth, email, password);
   }
 
   login({ email, password }: any) {
-
     this.sesionIniciada=true;
+    console.log(this.sesionIniciada)
     return signInWithEmailAndPassword(this.auth, email, password);
 
   }
 
+  login2(email: string, password: string) {
+
+
+
+    signInWithEmailAndPassword(this.auth,email, password)
+      .then((response) => {
+        firebase
+          .auth()
+          .currentUser?.getIdToken()
+          .then((token) => {
+            this.sesionIniciada = true;
+            this.token = token;
+
+            this.router.navigate(['']);
+          });
+      });
+  }
+
+  getSesion() {
+    return this.sesionIniciada;
+  }
+
+  getIdToken() {
+    return this.token;
+  }
+
+  estaLogueado() {
+    console.log("El token del user es " , this.token)
+    return this.token;
+  }
+
   loginWithGoogle() {
-    this.sesionIniciada=true;
+
     return signInWithPopup(this.auth, new GoogleAuthProvider());
+
+
+
+  }
+
+  logout2() {
+    firebase
+      .auth()
+      .signOut()
+      .then(() => {
+        this.token = '';
+
+        this.router.navigate(['']);
+      });
   }
 
   logout() {
-    this.sesionIniciada=false;
+    this.sesionIniciada = false;
     return signOut(this.auth);
   }
-
 }
